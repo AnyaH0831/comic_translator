0b6ea6b944d0358cc36503f5e0993cb0299dbb90
AnyaH0831 <anyahuang0831@gmail.com>
2026-02-16 03:36:34 -0500

    installations, fastapi backend, conenction between front and backend

---
+++ backend/server.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+ 
+class TranslateRequest(BaseModel):
+    image: str 
+
+@app.post("/translate")
+async def translate(request: TranslateRequest):
+    
+    print(request.image[:50])
+
+    returnBody = {
+        "status": "Received"
+    }
+    return returnBody
+
+# @app.get("/api/data")
+# def get_data():
+#     return {"message": "Hello from FastAPI backend!"}
--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,32 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     const images = document.querySelectorAll('img');
 
     const filteredImg = Array.from(images).filter(image => image.offsetWidth >= 300);
     
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = filteredImg[0].naturalWidth;
+    canvas.height = filteredImg[0].naturalHeight;
+
+    ctx.drawImage(filteredImg[0], 0, 0);
+
+    const base64 = canvas.toDataURL('image/jpeg').split(',')[1];
+    console.log(base64.substring(0,100));
+
     sendResponse({count: filteredImg.length});
     // console.log(filteredImg.length);
+
+    const response = await fetch('http://localhost:8000/translate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({image: base64})
+    });
+
+    const data = await response.json();
+    console.log(data);
 }) 
+
